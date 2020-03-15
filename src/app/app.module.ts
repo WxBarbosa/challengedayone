6c64f1240647efd4a766850668e67ee54467b610
wylkson <marcosduval123@gmail.com>
2020-03-14 21:50:21 -0300

    Initials commit: Add new modules bootstrap, models, shared components(*) and main components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgxBootstrapModule } from './modules/ngxbootstrap/ngxbootstrap.module';
+
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { BottomComponent } from './shared/bottom/bottom.component';
 
 @NgModule({
   declarations: [
+    HomeComponent,
+    HeaderComponent,
+    BottomComponent,
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    NgxBootstrapModule
   ],
   providers: [],
   bootstrap: [AppComponent]
